9b9fa31948744801666adce736076637c82f5154
Sachin Basnet <basnetsachin20@gmail.com>
2025-07-05 18:57:18 +0300

    first commit

---
+++ wp-content/plugins/realar-core/assets/js/realar.ajax5152.js
@@ -0,0 +1,45 @@
+/*
+
+[Main Script]
+
+Project: Realar
+Version: 1.0
+Author : Themeholy
+
+*/
+;(function($){
+    "use strict";
+/* ------------------------------------------------------------------------- *
+
+    * Mail Chimp ajax
+
+    * ------------------------------------------------------------------------- */
+
+    var $widgetsubscribeForm = $('form.newsletter-form');
+
+    // Subscribe Shortcode MailChimp ajax
+    $widgetsubscribeForm.on('submit',function(e){
+        let $emailAdd = $(this).find('input[type="email"]').val();
+        $.ajax({
+            type: 'POST',
+            url: realarajax.action_url,
+            data:{
+                sectsubscribe_email: $emailAdd,
+                security: realarajax.nonce,
+                action: 'realar_subscribe_ajax'
+            },
+
+            success: function(data){
+                $('form.newsletter-form input[type="email"]').val('');
+                $('.newsletter-form').append(data);
+            },
+
+            error: function(){
+                $('.newsletter-form').append('<div class="alert alert-danger mt-2" role="alert">Something Goes Wrong</div>');
+
+            }
+        });
+        e.preventDefault();
+    });
+
+})(jQuery);
